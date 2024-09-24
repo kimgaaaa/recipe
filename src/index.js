68892f67faac7f57b5cc37bde85a074fe29420c2
kimgaaaa <dl4525@naver.com>
2024-09-24 19:40:51 +0900

    레시피 메뉴

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Root from './layout/Root';
+import Home from './layout/Home';
+import Recipe from './layout/Recipe';
+import Category from './layout/Category';
+
+const router=createBrowserRouter([
+   {
+      path:'/',
+      element: <Root />,
+      children:[
+         {
+            index:true,
+            element: <Home />,   
+         },
+         {
+            path: 'recipe/:id',
+            element: <Recipe />,   
+         },
+         {
+            path: 'category/:category',
+            element: <Category />,   
+         },
+      ],
+   },
+]);
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RouterProvider router={router} />
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
