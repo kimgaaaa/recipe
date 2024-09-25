388d42fc645356ef55496fa1e14a38eb75286108
kimgaaaa <dl4525@naver.com>
2024-09-25 19:36:02 +0900

    동적링크

--- src/components/List.js
+++ src/components/List.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import './List.css';
+import {Link} from 'react-router-dom';
+import { AiFillThunderbolt } from "react-icons/ai";
+import { MdSoupKitchen } from "react-icons/md";
 
 const List = ({data}) => {
     return (
         <div className='group'>
             {
-                data.map(({RCP_SEQ, RCP_NM, RCP_WAY2, ATT_FILE_NO_MAIN}) => (
-                    <div className="list" ket={RCP_SEQ}>
-                        <img src={ATT_FILE_NO_MAIN} alt={RCP_NM} />
-                        <div className="list-text-wrap">
-                            <div className="list-txt-title">{RCP_NM}</div>
-                            <div className="list-txt-way">{RCP_WAY2}</div>
+                data.map(({RCP_SEQ, RCP_NM, RCP_WAY2, ATT_FILE_NO_MAIN, INFO_ENG}) => (
+                    <Link to={`recipe/${RCP_SEQ}`} key={RCP_SEQ}>
+                        <div className="list" ket={RCP_SEQ}>
+                            <img src={ATT_FILE_NO_MAIN} alt={RCP_NM} />
+                            <div className="list-text-wrap">
+                                <div className="list-txt-title">{RCP_NM}</div>
+                                <div className='list-sub-text-wrap'>
+                                    <div className="list-txt-kal"><span className='icon'><AiFillThunderbolt /></span>{INFO_ENG}Kcal</div>
+                                    <div className="list-txt-way"><span className='icon'><MdSoupKitchen /></span>{RCP_WAY2}</div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
+                    </Link>
                 ))
             }
             
